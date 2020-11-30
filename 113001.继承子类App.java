1c94fe91fc4a0b013917f0856fa9d8d0a0f1a496
1637936665 <74756259+1637936665@users.noreply.github.com>
2020-11-30 15:10:23 +0800

    Update 113001.继承子类App.java

--- 113001.继承子类App.java
+++ 113001.继承子类App.java
@@ -6,9 +6,9 @@ public class App extends R{
     public static void main(String[] args) {
         System.out.println(boy_filePath);
         System.out.println(girl_filePath);
-        int i=new Random().nextInt(2);
+        int i=new Random().nextInt(2);//随机数2以内的整数（不包括2）
         System.out.println(i);
-        System.out.println(array2d[i][new Random().nextInt(2)]);
+        System.out.println(array2d[i][new Random().nextInt(3)]);
 
     }
 }
